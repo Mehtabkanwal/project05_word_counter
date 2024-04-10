ea3c30962fb1b19d2066d1780f72e781c1d0cebd
Mehtabkanwal <mehtab.kanwal431@gmail.com>
2024-04-11 04:39:00 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.redBright.underline.bold.italic("\t\t ****************************************WELCOME TO MY WORD COUNTER APP***************************************************"))
+function wordCounter (text:string) {
+    let freeWhileSpace = text.replace(/\s/g,"");
+    return freeWhileSpace.length;
+}
+async function startCounter (wordCounter:(text:string)=> number) {
+    do{
+        let result = await inquirer.prompt(
+                {
+                    type:"input",
+                    message:chalk.greenBright.bold("write your text..."),
+                    name:"text"
+                }
+            
+        )
+        console.log(chalk.overline.bold.underline.blueBright("Your words count is: "))
+        console.log(chalk.yellowBright(wordCounter(result.text)));
+    }
+    while(true)
+    
+}
+ startCounter(wordCounter);
